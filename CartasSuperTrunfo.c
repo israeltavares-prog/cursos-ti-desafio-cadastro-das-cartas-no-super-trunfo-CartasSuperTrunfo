86b30aa4238fb4b34faccda49034b53f6573c3a7
israeltavares-prog <israelneto@hotmail.com>
2026-04-21 03:06:13 +0000

    ediçao um treino

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,17 +6,15 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    int main() {
+  float temperatura = 200;
 
-    return 0;
+  if (temperatura > 30.0) {
+    printf("Está calor\n");
+  } else {
+    printf("Não está calor\n");
+  }
+
+  return 0;
 }
+    
